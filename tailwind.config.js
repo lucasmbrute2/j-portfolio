255704424cc90b3dc63f71ac8ae531335abb0a77
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-24 23:17:37 -0300

    feat: add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,25 @@ module.exports = {
       fontFamily: {
         sans: 'var(--font-roboto)',
       },
+      maxWidth: {
+        'max-w-screen-1.5xl': '1444px',
+      },
       colors: {
         'cta-button': 'var(--cta-color)',
         primary: '#1E1E1E',
         secondary: '#FFFFFF',
+        purple: {
+          50: '#f3eefc',
+          100: '#d8cbf7',
+          200: '#c6b2f3',
+          300: '#ab8eee',
+          400: '#9b79ea',
+          500: '#8257e5',
+          600: '#764fd0',
+          700: '#5c3ea3',
+          800: '#48307e',
+          900: '#372560',
+        },
       },
       backgroundColor: {
         'bg-cta': 'var(--cta-color)',
