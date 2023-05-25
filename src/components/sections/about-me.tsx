30176e5809d3361618fd7e034f084b169d88a9b1
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-24 23:18:37 -0300

    feat: add aboutMeSection component

---
+++ src/components/sections/about-me.tsx
@@ -0,0 +1,34 @@
+import { ImageWindow } from '../img-window'
+import me from '../../assets/me.svg'
+import { Qualifications } from './qualifications'
+
+export function AboutMeSection() {
+  return (
+    <section className="block bg-gradient-to-b from-[#0f0e0e] to-[#0f172a] pt-16">
+      <div className="mx-auto max-w-max-w-screen-1.5xl py-36 lg:py-0">
+        <div className="flex flex-col items-center justify-between lg:flex-row">
+          <ImageWindow
+            src={me}
+            height="h-[456px]"
+            className="w-[350px] lg:w-[500px]"
+          />
+          <div className="relative w-full px-4 text-center text-secondary lg:w-[504px] lg:px-0 lg:text-left">
+            <h1 className=" text-[min(85px,10vw)] font-bold leading-[112px]">
+              Sobre mim
+            </h1>
+            <p className="text-[25px]  leading-8">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur necessitatibus exercitationem nam possimus corrupti
+              quam facilis, iusto praesentium quasi nesciunt, dolorum
+              voluptatibus soluta quibusdam ullam beatae sequi aperiam molestias
+              ea.
+            </p>
+            {/* Blur */}
+            <div className="absolute right-[1%] top-5 z-50 hidden h-[300px] w-[555px] rounded-full bg-purple-700 opacity-20 blur-2xl lg:block" />
+          </div>
+        </div>
+        <Qualifications />
+      </div>
+    </section>
+  )
+}
